01189d063381bc3bdcc9cc6a1214ef232479f949
michelle <mverone@gmail.com>
2017-03-08 22:28:22 -0800

    Create function to access DOM and replace specific words

--- background.js
+++ background.js
@@ -1,16 +1,4 @@
 
-//Aqui vamos gerenciar das Browser Actions.
-// Um exemplo de Browser Action: colocar um ícone ao lado da address bar, de modo que nosso
-// código responda a cliques dados ali.
-// Primeiro, temos de add uma imagem (icon.png) e inclui-la no arquivo 
-//manifest.jo.
-// O script content.js tem limitações e só gerencia ações na página atual.
-// Assim, outro tipo de script  precisa ser adicionado e que, em vez da página atual, acesse
-// outras coisas. Esse script será o background.js
-// Adicionar a Message passing é necessario pq o "content script" nao responde a clicks no Browser Action.
-// O Google tem a opção "message passing", que permite que os scritps enviem mensagens e respondam a elas.
-// Desse modo, content.js e background.js poderão interegir
-
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
@@ -18,10 +6,10 @@ chrome.browserAction.onClicked.addListener(function(tab) {
   });
 });
 
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "open_new_tab" ) {
-      chrome.tabs.create({"url": request.url});
-    }
-  }
-)
+// chrome.runtime.onMessage.addListener(
+//   function(request, sender, sendResponse) {
+//     if( request.message === "open_new_tab" ) {
+//       chrome.tabs.create({"url": request.url});
+//     }
+//   }
+// )
--- content.js
+++ content.js
@@ -1,28 +1,18 @@
 
+$(document).ready(function() { 
+  checkContainer();
+});
 
-// Acessar a primeira url e printa-la no console. 
-// Note que nao precisa checar se o dom está carregado, pq o chrome so
-// injeta os plugins depois que a pagina já carregou.
+var wordsToChange = [ /tech/g, /app/g, /chat/g, /tools/g ]
 
-// var firstHref = $("a[href^='http']").eq(0).attr("href");
-// console.log( "bloo");
-// console.log(firstHref);
-
-// Abaixo, respondemos à ação enviada por background.js
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-      console.log("blaaaa");
-      //console.log(firstHref);
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-    }
+function checkContainer() { 
+  if($("body").is(':visible')) {
+    for (i =0; i < wordsToChange.length; i++) {
+      $('body')[0].innerHTML = $('body')[0].innerHTML.replace(wordsToChange[i], "hhaaaaa")
+    } 
   }
-);
-
-// Para que a url desejada seja aberta, e como apenas o backround.js pode abrir urls, é preciso
-// o seguinte:
-// background.js nota que ouve um click no icone, envia uma msg pra content.js informando
-// que ouve um clique em x link; content.js pega esse link e manda de volta pra background.js,
-// mandando-o abrir o tal link.
-//
+  else {
+     console.log("setTimeout");
+     setTimeout(checkContainer, 1000);
+  }
+}
